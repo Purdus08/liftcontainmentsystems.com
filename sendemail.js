7fff279bc53d39ad1a4881d0479f554b94ce3039
Purdus08 <jordjajara@gmail.com>
2024-12-06 13:09:20 -0700

    Add files via upload

---
+++ sendemail.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+exports.handler = async () => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: 'Product Purchase Notification',
+      text: 'A user has purchased a product from your website!',
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email sent successfully!' }),
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Failed to send email.' }),
+    };
+  }
+};
