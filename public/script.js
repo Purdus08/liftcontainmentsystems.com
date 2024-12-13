cc724c2c62063791e266fbe13d427364114b3699
Purdus08 <jordjajara@gmail.com>
2024-12-12 21:02:15 -0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,16 @@
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
+
+
+/*document.getElementById('buyBtn').addEventListener('click', async () => {
+  try {
+    const response = await fetch('/.netlify/functions/sendEmail', { method: 'POST' });
+    const result = await response.json();
+    alert(result.message);
+  } catch (error) {
+    alert('Failed to send email. Please try again.');
+  }
+});*/
